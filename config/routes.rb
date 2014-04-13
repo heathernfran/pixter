7a95733e96f2969ef50fdb20a3f07a1e923269c6
Heather Frantisak <hfrantis@my.hpu.edu>
2014-04-13 00:42:17 +0000

    Added form to application

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Pixter::Application.routes.draw do
-  get "postsvagrant/up"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
